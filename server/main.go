a080e3926e84ad1ff8669fc7237c39cb4f9efa15
Edward <github@edwardcs.com>
2023-08-13 19:18:11 +0300

    Feat: create server

--- server/main.go
+++ server/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+}
 
 func main() {
-	fmt.Println("ChatOrBye v0.01")
+	fmt.Println("Server is running on port 8080")
+
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
 }
